5a9b32445b8e9e7bdd61f6013726ff13f0cc5816
toto1852 <toto1852@gmail.com>
2018-09-11 14:04:36 +0200

    version actuelle

--- src/main/java/fr/epf/demoseptembre/controllers/UsersController.java
+++ src/main/java/fr/epf/demoseptembre/controllers/UsersController.java
@@ -1,9 +1,12 @@
 package fr.epf.demoseptembre.controllers;
 
+import fr.epf.demoseptembre.models.User;
 import fr.epf.demoseptembre.persistence.UserDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 /**
  * TODO class details.
@@ -21,17 +24,45 @@ public class UsersController {
 
   /**
    * Ceci sera mappé sur l'URL '/users'.
-   * C'est le routeur de Spring MVC qui va détecter et appeler directement cette méthode.
-   * Il lui fournira un "modèle", auquel on pourra rajouter des attributs.
-   * Ce modèle sera ensuite forwardé à une page web (dans resources/templates).
-   * Le nom de la template est retourné par la fonction. Ici, elle appelle donc le template "users".
-   * @param model le modèle
+   *    * C'est le routeur de Spring MVC qui va détecter et appeler directement cette méthode.
+   *    * Il lui fournira un "modèle", auquel on pourra rajouter des attributs.
+   *    * Ce modèle sera ensuite forwardé à une page web (dans resources/templates).
+   *    * Le nom de la template est retourné par la fonction. Ici, elle appelle donc le template "users".
+   *    * @param model le modèle
    * @return
    */
+
   @GetMapping("/users")
   public  String getUsers (Model model) {
     model.addAttribute("data", userDao.findAll());
     return "users-list";
+  }
+
+  @GetMapping("/user")
+  public String addUsersPage (Model model) {
+    User usr = new User();
+    model.addAttribute(usr);
+    return "add_member";
+  }
+
+  @PostMapping("user")
+  public String addUsers (Model model, User usr){
+    userDao.save(usr);
+
+    return "redirect:/users";
+  }
+
+  @GetMapping("/index")
+          public String index (Model model)
+  {
+
+    return "index";
+  }
+
+  @PostMapping("index")
+    public String sendAddPromo (Model model)
+  {
+
   }
   
 }
